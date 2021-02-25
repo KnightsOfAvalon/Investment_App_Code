0459b74dd44df76bf203aac7808194edac466fe7
Ava Matthew <washington_ava@yahoo.com>
2021-02-25 16:37:11 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Our Investment App is Coming Soon!
         </p>
         <a
           className="App-link"
